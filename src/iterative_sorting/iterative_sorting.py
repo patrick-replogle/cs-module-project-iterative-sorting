d600cb19734a9d2346664767c1b7e021a6a799ab
patrick-replogle <patrickr1138@gmail.com>
2020-07-30 14:38:55 -0700

    implemented linear search, binary search, and bubble sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,3 +1,7 @@
+import time
+arr = [43, 2, 1, 77, 53, 54, 44, 6]
+
+
 # TO-DO: Complete the selection_sort() function below
 def selection_sort(arr):
     # loop through n-1 elements
@@ -8,7 +12,6 @@ def selection_sort(arr):
         # (hint, can do in 3 loc)
         # Your code here
 
-
         # TO-DO: swap
         # Your code here
 
@@ -17,10 +20,24 @@ def selection_sort(arr):
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
-    # Your code here
-
-
+    swaps = True
+    for i in range(len(arr)-1):
+        swaps = False
+        for j in range(1, len(arr)):
+            if arr[j] < arr[j-1]:
+                arr[j], arr[j-1] = arr[j-1], arr[j]
+                swaps = True
+        if swaps == False:
+            break
     return arr
+
+
+print('Bubble Sort')
+bubble_start = time.time()
+print(bubble_sort(arr))
+bubble_end = time.time()
+print(f"Runtime: {bubble_end - bubble_start}")
+
 
 '''
 STRETCH: implement the Counting Sort function below
@@ -31,16 +48,17 @@ data. The idea behind this algorithm then is that we can create "buckets"
 from 0 up to the max value. This is most easily done by initializing an
 array of 0s whose length is the max value + 1 (why do we need this "+ 1"?).
 
-Each buckets[i] then is responsible for keeping track of how many times 
+Each buckets[i] then is responsible for keeping track of how many times
 we've seen `i` in the input set of data as we iterate through it.
 Once we know exactly how many times each piece of data in the input set
-showed up, we can construct a sorted set of the input data from the 
-buckets. 
+showed up, we can construct a sorted set of the input data from the
+buckets.
 
 What is the time and space complexity of the counting sort algorithm?
 '''
+
+
 def counting_sort(arr, maximum=None):
     # Your code here
-
 
     return arr
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,42 @@
+import time
+
+arr = []
+for i in range(1000000):
+    arr.append(i)
+
+
 def linear_search(arr, target):
-    # Your code here
+    for num in arr:
+        if num == target:
+            return True
+    return -1
 
 
-    return -1   # not found
+print('Linear Search')
+linear_start = time.time()
+print(linear_search(arr, 9492))
+linear_end = time.time()
+print(f"Runtime: {linear_end - linear_start}")
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    l = 0
+    r = len(arr) - 1
 
-    # Your code here
+    while l <= r:
+        mid = (r + l) // 2
+        if arr[mid] == target:
+            return True
+        elif arr[mid] > target:
+            r = mid - 1
+        elif arr[mid] < target:
+            l = mid + 1
+    return -1
 
 
-    return -1  # not found
+print('Binary Search')
+binary_start = time.time()
+print(binary_search(arr, 5930))
+binary_end = time.time()
+print(f"Runtime: {binary_end - binary_start}")
